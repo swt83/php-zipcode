e27be1ff64e00d15a3da296682dcd59e43a03376
Scott Travis <scott@swt83.com>
2017-09-14 21:27:25 +0000

    Improve method.

--- src/Zipcode.php
+++ src/Zipcode.php
@@ -17,31 +17,46 @@ class Zipcode
 
 	public function __construct($str)
 	{
+		// explode at dash
 		$parts = explode('-', $str);
 
+		// if dash found...
 		if (sizeof($parts) > 1)
 		{
+			// save parts
 			$this->five = $parts[0];
 			$this->four = $parts[1];
 		}
 		else
 		{
+			// get string length
 			$len = strlen($str);
 
+			// if length is 5 or less...
 			if ($len <= 5)
 			{
+				// just save 5
 				$this->five = $str;
 			}
+
+			// else if length is more than 5...
 			else
 			{
+				// save both
 				$this->four = substr($str, -4);
 				$this->five = substr($str, 0, -4);
 			}
 		}
 
+		// strip non numbers
+		$this->five = $this->five ? preg_replace('/\D/', '', $this->five) : null;
+		$this->four = $this->five ? preg_replace('/\D/', '', $this->four) : null;
+
+		// add padding zeroes
 		$this->five = $this->five ? (string) str_pad($this->five, 5, '0', STR_PAD_LEFT) : null;
 		$this->four = $this->four ? (string) str_pad($this->four, 4, '0', STR_PAD_LEFT) : null;
 
+		// finalize
 		$this->full = (string) $this->four ? $this->five.'-'.$this->four : $this->five;
 	}
 }
